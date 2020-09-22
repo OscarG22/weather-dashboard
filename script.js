1d4dbcff1648375a3a6419ae915002ae93538df0
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-09-22 16:09:04 -0700

    Added city temperature.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // variables
 let apiKey = "c84eb4f0d313c91844853c334d0ba345";
+let url = "http://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=c84eb4f0d313c91844853c334d0ba345&units=imperial"
+let currentCity = $("#currentcity");
+
+
+$.ajax({
+    url: url,
+    method: "GET"
+}).then(function (response) {
+    currentCity.text(response.main.temp);
+    console.log(response.main.temp);
+    
+});
