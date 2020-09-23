4f181738e42de52d8e4723eb3f5bb2a4b352e621
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-09-22 17:11:24 -0700

    Added search button.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let apiKey = "c84eb4f0d313c91844853c334d0ba345";
 let url = "http://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=c84eb4f0d313c91844853c334d0ba345&units=imperial"
 let currentCity = $("#currentcity");
+let cityHumidity = $("#cityhumidity");
 
 
 $.ajax({
@@ -10,5 +11,14 @@ $.ajax({
 }).then(function (response) {
     currentCity.text(response.main.temp);
     console.log(response.main.temp);
-    
+
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        currentCity.text(response.main.humidity);
+        console.log(response.main.humidity);
+
+    });
 });
+
